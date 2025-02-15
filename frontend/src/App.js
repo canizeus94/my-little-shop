9cd579c9c3381964ea2493617f702792750cd620
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-14 22:49:54 -0500

    Created Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
+  // Return the Header, main content, and Footer.
   return (
-    // Wrap the Header component in the App component. Fragment is used to wrap multiple elements without adding extra nodes to the DOM.
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to My Little Shop Charlotte</h1>
+          <h1>Welcome to My Little Shop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   )
 }
