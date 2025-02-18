2d9f82ac2faa1cf5b742b2dcf3192db985d93b6e
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-17 20:10:48 -0500

    Implemented React Router & Optimized Links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-    {/*Get the current year to display in the footer.*/}
-    const currentYear = new Date().getFullYear();
+  {/*Get the current year to display in the footer.*/}
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-        <Container>
-            <Row>
-                <Col className='text-center py-3'>
-                    <p>&copy; My Little Shop {currentYear}</p>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>&copy; My Little Shop {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart , FaUser} from 'react-icons/fa';
-import logo from '../assets/logo2.png';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo2.png";
 
 const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-            {/*Create Container to center content and prevent browser stretching.*/}
-            <Container>
-            <Navbar.Brand href="/">
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        {/*Create Container to center content and prevent browser stretching.*/}
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
               <img src={logo} alt="My Little Shop" />
               My Little Shop
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto"> {/*ms-auto to push items to the right.*/}
-                <Nav.Link href="/cart"><FaShoppingCart /> My Cart</Nav.Link>
-                <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {/*ms-auto to push items to the right.*/}
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> My Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
