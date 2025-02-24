9d12f80c1a70596a522e0155e3a72bdc685d4a03
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-23 22:54:52 -0500

    Create The Schema For Product, Review, User & Order

---
+++ backend/models/reviewModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+// Define the review schema.
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default reviewSchema;
