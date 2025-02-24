58850650349fe0b7eeef17a1f18d9f6264cbdf7e
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-23 20:17:47 -0500

    Renamed Environment Variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Connect to MongoDB Database.
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
