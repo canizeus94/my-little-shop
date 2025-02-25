9dcef30785ca2aa81960a4e5a7317cf7b24645c6
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-25 15:45:05 -0500

    Created Product Route & Implemented AsyncHandler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+// import products from '../data/products.js';
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/Product.js";
+
+const router = express.Router();
+
+// Route to get all products.
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// Route to get a product by ID.
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    // Return the product if found, otherwise return a 404 error.
+    if (product) {
+      return res.json(product);
+    }
+
+    res.status(404).json({ message: "Product not found" });
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file.
-import connectDB from './config/db.js'; // Import the database connection function.
-import products from './data/products.js';
+import connectDB from "./config/db.js"; // Import the database connection function.
+import productRoutes from "./routes/productRoutes.js"; // Import product routes.
 
 // Port number for the server.
 const port = process.env.PORT || 5000;
@@ -11,24 +11,12 @@ connectDB();
 // Create an instance of the Express application
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running');
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-// Endpoint to get all products.
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-// Endpoint to get a product by ID.
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: 'Product not found!' });
-  }
-});
+// Use the product routes for handling requests to /api/products
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
