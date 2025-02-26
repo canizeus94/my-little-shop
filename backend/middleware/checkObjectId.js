076f6c37fe7e05854b9669f1790e914334e41421
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-25 19:17:37 -0500

    Added Check For Invalid Object Ids

---
+++ backend/middleware/checkObjectId.js
@@ -0,0 +1,22 @@
+//@ts-check
+import { isValidObjectId } from "mongoose";
+
+/*
+ * Checks if the provided ID is a valid MongoDB ObjectId.
+ ******************************************************************************
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ * @param {import('express').NextFunction} next - The next middleware function.
+ * @throws {Error} - Throw error if the ObjectId is not valid.
+ ********************************************************************************
+*/
+
+function checkObjectId(req, res, next){
+    if (!isValidObjectId(req.params.id)) {
+        res.status(400);
+        throw new Error(`The Object Id: ${req.params.id} is not a valid Object Id.`);
+    }
+    next();
+}
+
+export default checkObjectId;
