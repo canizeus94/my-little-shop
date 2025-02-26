d9260c6f6cd51bb081715e7952f206cf9b419059
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-26 03:56:25 -0500

    Created Api Slice & Modified store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
 
 // Create a Redux store using Redux Toolkit.
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    // Add the apiSlice reducer to the store.
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  // Add the apiSlice middleware to the store.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  // Enable Redux DevTools extension if available.
+  devTools: true
 });
 
 export default store;
