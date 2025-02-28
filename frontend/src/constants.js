0ead2e3668e170ea91fd2239247da1967dbad444
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-27 22:52:41 -0500

    Created useGetProductDetailsQuery

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,6 @@
 // This file contains the constants used in the application.
-export const BASE_URL = '';
+export const BASE_URL = process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '/';
+// export const BASE_URL = ''; //If using proxy uncomment this line.
 export const PRODUCTS_URL = 'api/products';
 export const USERS_URL = 'api/users';
 export const ORDERS_URL = 'api/orders';
