6cb98937dde25067400ce710010e0eb3a73e4b52
canizeus94 <ramirezestudilloe@gmail.com>
2025-02-28 18:05:44 -0500

    Added addToCart Reducer Function To cartSlice.js

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -7,7 +7,20 @@ const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getI
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+            const itemExists = state.cartItems.find((x) => x._id === item._id );
+            
+            // If the item exists in our cartItems state return it.
+            if(itemExists) {
+                state.cartItems = state.cartItems.map((x) => x._id === itemExists._id ? item : x);
+            } else {
+                // Add the state item to the cartItems state.
+                state.cartItems = [...state.cartItems, item];
+            }
+        },
+    },
 });
 
 export default cartSlice.reducer;
