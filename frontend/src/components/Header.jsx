d1c7498c472c7bf024c3d733a3c53a7d9c987c9b
canizeus94 <ramirezestudilloe@gmail.com>
2025-03-01 15:14:24 -0500

    Added Badge To Header.jsx To Display Cart Qty

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { useNavigate, Link } from "react-router-dom";
-import logo from "../assets/logo2.png";
+import React from 'react';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useNavigate, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import logo from '../assets/logo2.png';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -19,7 +22,12 @@ const Header = () => {
             <Nav className="ms-auto">
               {/*ms-auto to push items to the right.*/}
               <Nav.Link as={Link} to="/cart">
-                <FaShoppingCart /> My Cart
+                <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
               </Nav.Link>
               <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
