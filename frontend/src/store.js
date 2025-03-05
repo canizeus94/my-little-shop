904e1b72302c7c285ea8548d6f28ffc22eaaed43
canizeus94 <ramirezestudilloe@gmail.com>
2025-03-04 23:45:46 -0500

    Created Users Api Slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authReducer from './slices/authSlice';
 
-// Create a Redux store using Redux Toolkit.
+/**
+ * Create a Redux store using Redux Toolkit.
+ * apiSlice, cartSlice, & authReducers are the custom
+ * reducers added to the store.
+ */
 const store = configureStore({
   reducer: {
-    // Add the apiSlice reducer to the store.
     [apiSlice.reducerPath]: apiSlice.reducer,
-    // Add the cartSlice reducer to the store.
     cart: cartSliceReducer,
+    auth: authReducer,
   },
   // Add the apiSlice middleware to the store.
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-  // Enable Redux DevTools extension if available.
-  devTools: true
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
