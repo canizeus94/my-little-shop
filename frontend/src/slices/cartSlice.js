6b33921874de039e48f8d1d98f3a8850350fb8ee
canizeus94 <ramirezestudilloe@gmail.com>
2025-03-08 14:46:59 -0500

    Created Shipping Screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,7 +4,7 @@ import { updateCart } from '../utils/cartUtils';
 // Check if there is already an instance of cart.
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 // Create the Cart Slice.
 const cartSlice = createSlice({
@@ -15,13 +15,13 @@ const cartSlice = createSlice({
       const item = action.payload;
       const itemExists = state.cartItems.find((x) => x._id === item._id);
 
-      // If the item exists in our cartItems state return it.
+      // If the item exists in our cartItems state return it
+      // else add the cartItems to state.
       if (itemExists) {
         state.cartItems = state.cartItems.map((x) =>
           x._id === itemExists._id ? item : x
         );
       } else {
-        // Add the state item to the cartItems state.
         state.cartItems = [...state.cartItems, item];
       }
       return updateCart(state);
@@ -30,12 +30,25 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
-    // NOTE: here we need to reset state for when a user logs out so the next
-    // user doesn't inherit the previous users cart and shipping
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+    // Reset state to prevent next user from inheriting previous users state.
     resetCart: (state) => (state = initialState),
   },
 });
 
-export const { addToCart, removeFromCart, resetCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  resetCart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
